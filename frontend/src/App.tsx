34912927b009708a9cbf34b975a9b9701d8b4be3
Gabriel Domingues <gabrieldomingues123@hotmail.com.br>
2022-11-17 13:48:19 -0300

    Outros componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import NotificattionButton from "./components/NotificattionButton"
+
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 
 function App() {
   return (
     <>
-      <h1>Olá Mundo!</h1>
-      <NotificattionButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
